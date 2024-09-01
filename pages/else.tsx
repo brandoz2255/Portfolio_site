2ff505e70248207c9b91f7a086e4ab55a15f159e
Dulc3 <124217011+brandoz2255@users.noreply.github.com>
2024-08-31 20:49:20 -0700

    first commit will work on it more !

---
+++ pages/else.tsx
@@ -0,0 +1,16 @@
+
+
+
+export default function ElsePage(){
+// add the components to this page mf!! 
+return(
+    <main>
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className="mb-3 text-2xl font-semibold">Hello!{" "}</h1>
+            <p>This is my custom built page mf!</p>
+        </div>
+    </main>
+
+    );
+
+}
