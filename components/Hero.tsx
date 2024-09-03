4453c49da58195040cb571412915ecf9afc04e6c
Dulc3 <124217011+brandoz2255@users.noreply.github.com>
2024-09-03 16:42:48 -0700

    added a div

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,25 +4,33 @@ import { Spotlight } from './ui/Spotlight'
 const Hero = () => {
   return (
     <div className="pb-20 pt-36 relative">
-      <div>
-        <Spotlight className="z-30 -top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
-        <Spotlight className="z-24 -top-10 -left-full h-[80vh] w-[50vw]" fill="purple" />
-        <Spotlight className="z-10 -top-28 -left-80 h-[80vh] w-[50vw]" fill="blue" />
+      <div className='relative'>
+        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
+        <Spotlight className="-top-10 -left-full h-[80vh] w-[50vw]" fill="purple" />
+        <Spotlight className="-top-28 -left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
 
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
-      {/* Radial gradient for the container to give a faded look */}
-      <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+      <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.3] bg-grid-black/[0.2] absolute flex items-center justify-center overflow-hidden">
+       {/* Radial gradient for the container to give a faded look */}
+       <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
 
       <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
         Backgrounds
       </p>
     </div>
+    
+    <div className="flex justify-center relative my-20 z-10">
+      <div className='max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center'>
+          <h2 className='uppercase tracking-widest text-xs text-center -text-blue-100 max-w-80'>
+            Next.js something!
+          </h2>
+        </div>
+      </div>
     </div>
     
     
   )
 }
 
-export default React.memo(Hero)
+export default Hero
